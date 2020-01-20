bfab51ca52ae0ad56f1eeb0543abc87ce0b25da4
Tachibana446 <stinger33z@gmail.com>
2020-01-20 23:12:35 +0900

    座標アトリビュートはk3Floatで良さそう

--- distanceTerrainNode.py
+++ distanceTerrainNode.py
@@ -15,6 +15,7 @@ class distanceTerrainNode(om.MPxNode):
     id = om.MTypeId(0x7f002)
     name = 'distanceTerrainNode'
     terrain = om.MObject()
+    jointPos = om.MObject()
     distance = om.MObject()
 
     def __init__(self):
@@ -31,25 +32,37 @@ class distanceTerrainNode(om.MPxNode):
             'terrain', 'terrain', om.MFnData.kMesh, om.MObject.kNullObj)
         nAttr.storable = True
         nAttr.writable = True
+        nAttr = om.MFnNumericAttribute()
+        distanceTerrainNode.jointPos = nAttr.create(
+            'jointPosition', 'jointPos', om.MFnNumericData.k3Float, 0
+        )
+        nAttr.storable = True
+        nAttr.writable = True
 
         distanceTerrainNode.addAttribute(distanceTerrainNode.terrain)
+        distanceTerrainNode.addAttribute(distanceTerrainNode.jointPos)
 
     def compute(self, plug, dataBlock):
         return
 
+
+# ノード名、ID, creater関数, Initialize関数, ノードの種類を登録 
 def initializePlugin(obj):
     mplugin = om.MFnPlugin(obj)
     try:
         mplugin.registerNode(distanceTerrainNode.name, distanceTerrainNode.id,
                              distanceTerrainNode.creator, distanceTerrainNode.initialize, om.MPxNode.kDependNode)
     except:
-        sys.stderr.write('Failed to register node: %s' % distanceTerrainNode.name)
+        sys.stderr.write('Failed to register node: %s' %
+                         distanceTerrainNode.name)
         raise
+
 
 def uninitializePlugin(obj):
     mplugin = om.MFnPlugin(obj)
     try:
         mplugin.deregisterNode(distanceTerrainNode.id)
     except:
-        sys.stderr.write('Failed to uninitialize node: %s' % distanceTerrainNode.name)
+        sys.stderr.write('Failed to uninitialize node: %s' %
+                         distanceTerrainNode.name)
         pass
